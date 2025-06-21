a144ffe3a65bc824b24dab7f9297706255356afb
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-06-21 10:12:33 +0200

    Nagłówek wersji B

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+COLOR_RESET = '\033[0m'
+
+COLOR_BLACK = '\033[90m'
+COLOR_RED = '\033[91m'
+COLOR_GREEN = '\033[92m'
+COLOR_YELLOW = '\033[93m'
+COLOR_BLUE = '\033[94m'
+COLOR_MAGENTA = '\033[95m'
+COLOR_CYAN = '\033[96m'
+COLOR_WHITE = '\033[97m'
+
 def main():
-    print("Hejka, Git!")
+    print(COLOR_BLUE + "Hejka, Git!" + COLOR_RESET)
     print("Fajnie, że wpadłeś!")
 
 if __name__ == "__main__":
