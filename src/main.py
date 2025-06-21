5fc744b37b80675bf8c4012cd447c7f4c6171ead
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-06-21 10:57:06 +0200

    fix: poprawiono powitanie z hejka na dzień dobry

--- src/main.py
+++ src/main.py
@@ -1,16 +1,5 @@
-COLOR_RESET = '\033[0m'
-
-COLOR_BLACK = '\033[90m'
-COLOR_RED = '\033[91m'
-COLOR_GREEN = '\033[92m'
-COLOR_YELLOW = '\033[93m'
-COLOR_BLUE = '\033[94m'
-COLOR_MAGENTA = '\033[95m'
-COLOR_CYAN = '\033[96m'
-COLOR_WHITE = '\033[97m'
-
 def main():
-    print(COLOR_RED + "Hejka, Git! Wersja A + " + COLOR_BLUE + "Wersja B połączone" + COLOR_RESET)
+    print("Dzień dobry, Git!")
     print("Fajnie, że wpadłeś!")
 
 if __name__ == "__main__":
